158db5d7385b2ffbdbf685345466fe3f3f930a0c
EduhGarcia <eg836151@gmail.com>
2024-09-04 18:33:54 -0300

    Teste tecnico

---
+++ src/scripts/main.js
@@ -0,0 +1,53 @@
+const bntFibonacci = document.querySelector("#btn-verify")
+const bntSearchLetter = document.querySelector("#btn-verify-string")
+
+bntFibonacci.addEventListener('click', () => { 
+    const num = document.querySelector('.number-user').value
+    let containerRes = document.querySelector('.res-febonacci')
+    const responseFibonacci = fibonacci(Number(num))
+
+    if (responseFibonacci) {
+        containerRes.innerHTML = `
+        <p>O número ${num} pertence a sequência de fibonacci</p>
+        `
+    } else {
+        containerRes.innerHTML = `
+        <p>O número ${num} não pertence a sequência de fibonacci</p>
+        `
+    }
+})
+
+bntSearchLetter.addEventListener('click', () => {
+    const stringUser = document.querySelector('.string-user').value
+    const containerRes = document.querySelector('.res-quantity-string')
+
+    let count = 0
+
+    for (const char of stringUser.toLowerCase()) {
+        if(char === 'a') {
+            count++ 
+        }
+    }
+
+    if (count != 0) {
+        containerRes.innerHTML = `
+        Na palavra contém ${count} letra(s) A
+        `
+    } else {
+        containerRes.innerHTML = `
+        Não contém letra A na palavra
+        `
+    }
+})
+
+function calcRoot(calc) {
+    const raiz = Math.round(Math.sqrt(calc))
+    
+    return raiz*raiz === calc
+}
+
+function fibonacci(n) {
+    if (calcRoot(5*n*n - 4) || calcRoot(5*n*n + 4)) return true
+    
+    return false
+}
